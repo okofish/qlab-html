dc4fcc28e01bc6af10901ce06057a21e7d79e78d
Jesse Friedman <jesse@jesse.ws>
2017-07-13 22:25:45 -0400

    Adding experimental functionality to render directly to PDF by connecting to a headless Chrome instance

--- cli.js
+++ cli.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 
 var argv = require('commander');
+var fs = require('fs');
 var opn = require('opn');
-var qlabPdf = require('./index.js');
+var path = require('path');
+var qlabHTML = require('./index.js');
 var readPkg = require('read-pkg');
 
 argv
   .description('Exports contents of the current QLab workspace to an HTML file')
-  .option('-o, --output [filename]', 'File to save HTML to (defaults to <workspace name>.html; use - to echo to stdout)')
+  .option('-o, --output [filename]', 'File to save HTML/PDF to (defaults to <workspace name>.html; use - to echo to stdout)')
   .option('-O, --open', 'Open HTML file in default browser after saving')
   .option('-n, --show-notes', 'Show cue notes')
   .option('-d, --dark', 'Use dark theme')
@@ -15,6 +17,8 @@ argv
   .option('-b, --indicate-broken-cues', 'Indicate broken cues with a red X')
   .option('-s, --hide-status', 'Hide cue status')
   .option('-t, --truncate-cue-names', 'Truncate cue names instead of wrapping them (can cause issues with printing)')
+  .option('--export-pdf', 'Render workspace to PDF instead of HTML (experimental; requires chrome-remote-interface and chrome-runner)')
+  .option('--landscape', 'Render PDF in landscape mode instead of portrait')
   .version(readPkg.sync(__dirname).version)
   .parse(process.argv);
 
@@ -22,16 +26,42 @@ var options = argv.opts();
 options.showStatus = !options.hideStatus;
 
 if (options.output === '-') {
-  qlabPdf.generateHTML(options, console.log);
+  qlabHTML.generateHTML(options, function(html) {
+    if (options.exportPdf === true) {
+      qlabHTML.convertHTMLtoPDF(html, options.landscape, function(pdf) {
+        console.log(pdf.toString());
+      });
+    } else {
+      console.log(html);
+    }
+  });
 } else {
-  qlabPdf.exportHTML(options.output, options, function(err, path) {
+  qlabHTML.exportHTML(options.output, options, function(err, htmlPath, html) {
     if (!err) {
-      console.log(`Workspace saved to ${path}`);
-      if (options.open === true) {
-        opn(path, {
-          wait: false
+      if (options.exportPdf === true) {
+        qlabHTML.convertHTMLtoPDF(html, options.landscape, function(pdf) {
+          var pathObj = path.parse(htmlPath);
+          pathObj.ext = '.pdf';
+          pathObj.base = undefined;
+          var pdfPath = path.format(pathObj);
+          fs.unlinkSync(htmlPath);
+          fs.writeFileSync(pdfPath, pdf);
+          console.log(`Workspace in PDF saved to ${pdfPath}`);
+          if (options.open === true) {
+            opn(pdfPath, {
+              wait: false
+            });
+          }
         });
+      } else {
+        console.log(`Workspace saved to ${htmlPath}`);
+        if (options.open === true) {
+          opn(filePath, {
+            wait: false
+          });
+        }
       }
+      
     } else {
       console.error('An error occurred while saving the HTML file.');
       console.error(err);
--- index.js
+++ index.js
@@ -27,8 +27,70 @@ module.exports.exportHTML = function(path, options, cb) {
     }
     fs.writeFile(path, html, function(err) {
       if (cb) {
-        cb(err, path);
+        cb(err, path, html);
       }
     });
   });
+}
+module.exports.convertHTMLtoPDF = function(html, landscape, cb) {
+  try {
+    var CDP = require('chrome-remote-interface');
+  } catch (err) {
+    CDP = null;
+  }
+  try {
+    var chromeRunner = require('chrome-runner');
+  } catch (err) {
+    chromeRunner = null;
+  }
+
+  if (!CDP) {
+    console.error('chrome-remote-interface not installed, please run `npm install chrome-remote-interface`');
+  } else if (!chromeRunner) {
+    console.error('chrome-runner not installed, please run `npm install chrome-runner`');
+  } else {
+    chromeRunner.launchWithHeadless().then(function(runner) {
+      CDP({
+        port: runner.port
+      }, function(client) {
+        var Network = client.Network;
+        var Page = client.Page;
+
+        // enable events then start!
+        Promise.all([
+          Page.enable()
+        ]).then(function() {
+          return Page.navigate({
+            url: 'about:blank'
+          })
+        }).then(function(results) {
+          var frameId = results.frameId;
+          return Page.setDocumentContent({
+            frameId: frameId,
+            html: html
+          })
+        }).then(function() {
+          return Page.printToPDF({
+            landscape: landscape || false,
+            displayHeaderFooter: false,
+            printBackground: true/*,
+            marginTop: 0,
+            marginBottom: 0,
+            marginLeft: 0,
+            marginRight: 0*/
+          })
+        }).then(function(pdf) {
+          cb(Buffer.from(pdf.data, 'base64'));
+          client.close();
+          runner.kill();
+        }).catch(function(err) {
+          console.error(err);
+          client.close();
+        });
+      }).on('error', function(err) {
+        console.error(err);
+      });
+    });
+
+  }
 }
